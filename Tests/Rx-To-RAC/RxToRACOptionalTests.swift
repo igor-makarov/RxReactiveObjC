b7266bceea8f493f7edb5238c96314e8b94ba9d9
Igor Makarov <igormaka@gmail.com>
2021-03-24 16:17:53 +0200

    add tests

---
+++ Tests/Rx-To-RAC/RxToRACOptionalTests.swift
@@ -0,0 +1,113 @@
+//
+//  RAC-To-Rx.swift
+//
+//
+
+import XCTest
+import ReactiveObjC
+import RxSwift
+import RxReactiveObjC
+
+class RxToRACOptionalTests: XCTestCase {
+    var disposable: RACScopedDisposable?
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        disposable = nil
+    }
+    
+    func test_Next() {
+        for count in [1, 2, 10] {
+            let arrayOfOptionals = Array(0..<count).map { "\($0)" as NSString }.flatMap { [$0, nil] }
+            let observable: Observable<NSString?> = Observable.from(arrayOfOptionals)
+            let values = expectation(description: "value")
+            values.expectedFulfillmentCount = count * 2
+            var count = 0
+            
+            disposable = observable.asSignal()
+                .subscribeNext { value in
+                    if count % 2 == 0 {
+                        XCTAssertNotNil(value)
+                    } else {
+                        XCTAssertNil(value)
+                    }
+                    count += 1
+                    values.fulfill()
+                }
+                .asScopedDisposable()
+            wait(for: [values], timeout: 2)
+        }
+    }
+    
+    func test_Completed() {
+        let observable: Observable<NSString?> = Observable<NSString?>.empty()
+        let completion = expectation(description: "completion")
+        disposable = observable.asSignal()
+            .subscribeCompleted { completion.fulfill() }
+            .asScopedDisposable()
+        wait(for: [completion], timeout: 2)
+    }
+
+    func test_NoError() {
+        let observable: Observable<NSString?> = Observable<NSString?>.empty()
+        let completion = expectation(description: "completion")
+        let errored = expectation(description: "errored")
+        errored.isInverted = true
+        disposable = observable.asSignal()
+            .subscribeError({ _ in errored.fulfill() },
+                            completed: { completion.fulfill() })
+            .asScopedDisposable()
+        wait(for: [completion, errored], timeout: 2)
+    }
+
+    func test_Error() {
+        let observable: Observable<NSString?> = Observable<NSString?>.error(NSError(domain: "Error", code: 2, userInfo: nil))
+
+        let errored = expectation(description: "errored")
+        disposable = observable.asSignal()
+            .subscribeError { error in
+                XCTAssertNotNil(error)
+                XCTAssertEqual(error! as NSError, NSError(domain: "Error", code: 2, userInfo: nil))
+                errored.fulfill()
+            }
+            .asScopedDisposable()
+        wait(for: [errored], timeout: 2)
+    }
+
+    func test_Dispose() {
+        let dispose = expectation(description: "dispose")
+        let observable: Observable<NSString?> = Observable<NSString?>.create { observer in
+            observer.onCompleted()
+            return Disposables.create {
+                XCTAssertNotNil(observer)
+                dispose.fulfill()
+            }
+        }
+
+        disposable = observable.asSignal()
+            .subscribeCompleted { }
+            .asScopedDisposable()
+        wait(for: [dispose], timeout: 1)
+    }
+
+    func test_NotDispose() {
+        let dispose = expectation(description: "dispose")
+        dispose.isInverted = true
+        let observable: Observable<NSString?> = Observable<NSString?>.create { observer in
+            return Disposables.create {
+                XCTAssertNotNil(observer)
+                dispose.fulfill()
+            }
+        }
+
+        disposable = observable.asSignal()
+            .subscribeCompleted { }
+            .asScopedDisposable()
+        wait(for: [dispose], timeout: 3)
+    }
+}
